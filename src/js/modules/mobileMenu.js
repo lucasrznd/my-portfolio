0e42528688e94b15b65e0a2b239c32e631de864c
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-22 09:01:14 -0300

    feat: adicionar animacao no menu mobile

--- src/js/modules/mobileMenu.js
+++ src/js/modules/mobileMenu.js
@@ -2,14 +2,29 @@
 export function initMobileMenu() {
     const mobileBtn = document.getElementById("mobile-btn");
     const mobileMenu = document.getElementById("mobile-menu");
+    const mobileNavItems = document.querySelectorAll('#mobile-nav-list .nav-item a');
+    const mobileIcon = mobileBtn.querySelector("i"); 
 
     mobileBtn.addEventListener("click", function () {
         mobileMenu.classList.toggle("active");
 
-        const icon = mobileBtn.querySelector("i");
-        if (icon) {
-            icon.classList.toggle("fa-bars");
-            icon.classList.toggle("fa-x");
+        if (mobileMenu.classList.contains("active")) {
+            mobileIcon.classList.remove("fa-bars");
+            mobileIcon.classList.add("fa-x");
+        } else {
+            mobileIcon.classList.remove("fa-x");
+            mobileIcon.classList.add("fa-bars");
         }
+
+        mobileBtn.classList.toggle("open");
+    });
+
+    mobileNavItems.forEach(item => {
+        item.addEventListener("click", () => {
+            mobileMenu.classList.remove("active");
+            mobileIcon.classList.remove("fa-x");
+            mobileIcon.classList.add("fa-bars");
+            mobileBtn.classList.remove("open"); 
+        });
     });
 }

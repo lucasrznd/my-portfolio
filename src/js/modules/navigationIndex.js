255c2b366b68b986f132f180288fa99fc85452b9
Lucas Rezende <lucasffrezende@gmail.com>
2025-01-22 08:00:48 -0300

    refactor(nav): alterar js e adicionar sombra dinamica

--- src/js/modules/navigationIndex.js
+++ src/js/modules/navigationIndex.js
@@ -1,25 +1,29 @@
 export function initNavigationIndex() {
     const sections = document.querySelectorAll('section');
     const navItems = document.querySelectorAll('.nav-item');
+    const header = document.querySelector('header');
 
     window.addEventListener('scroll', () => {
-        let currentSection = '';
+        let currentSectionIndex = 0; 
+        const scrollPosition = window.scrollY; 
 
-        // Identifica a seção visível
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop - 50; // Compensação caso tenha header fixo
-            const sectionHeight = section.clientHeight;
-            if (scrollY >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
-                currentSection = section.getAttribute('id');
+        // Adiciona ou remove sombra no header
+        if (scrollPosition <= 0) {
+            header.style.boxShadow = 'none';
+        } else {
+            header.style.boxShadow = '5px 1px 5px rgba(0, 0, 0, 0.1)';
+        }
+
+        sections.forEach((section, index) => {
+            const sectionTop = section.offsetTop - 96; 
+            const sectionBottom = sectionTop + section.offsetHeight;
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                currentSectionIndex = index; 
             }
         });
 
-        // Atualiza a classe 'active' no menu
-        navItems.forEach(item => {
-            item.classList.remove('active');
-            if (item.querySelector(`a[href="#${currentSection}"]`)) {
-                item.classList.add('active');
-            }
-        });
+        navItems.forEach(item => item.classList.remove('active'));
+        navItems[currentSectionIndex].classList.add('active');
     });
 }
